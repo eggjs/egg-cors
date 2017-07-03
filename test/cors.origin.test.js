433280572b0ffab4b724d81e0f665e259dd53c07
brick <brick.c.yang@gmail.com>
2017-07-03 17:45:57 +0800

    add test for config.cors.origin

---
+++ test/cors.origin.test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const assert = require('assert');
+const request = require('supertest');
+const mm = require('egg-mock');
+
+describe.only('test/cors.origin.test.js', () => {
+  let app;
+  before(() => {
+    app = mm.app({
+      baseDir: 'apps/cors.origin',
+    });
+    return app.ready();
+  });
+
+  after(() => app.close());
+
+  afterEach(mm.restore);
+
+  it('should not set `Access-Control-Allow-Origin` when request Origin header missing', () => {
+    return request(app.callback())
+      .get('/')
+      .expect({ foo: 'bar' })
+      .expect(res => {
+        assert(!res.headers['access-control-allow-origin']);
+      })
+      .expect(200);
+  });
+
+  it('should set `Access-Control-Allow-Origin` to request origin header', () => {
+    return request(app.callback())
+      .get('/')
+      .set('Origin', 'http://eggjs.org')
+      .expect('Access-Control-Allow-Origin', 'eggjs.org')
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect({ foo: 'bar' })
+      .expect(200);
+  });
+
+  it('should set `Access-Control-Allow-Origin` on POST request', () => {
+    app.mockCsrf();
+    return request(app.callback())
+      .post('/')
+      .set('Origin', 'http://eggjs.org')
+      .expect('Access-Control-Allow-Origin', 'eggjs.org')
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect(200);
+  });
+
+  it('should set `Access-Control-Allow-Origin` equal to the config not the white list', () => {
+    app.mockCsrf();
+    return request(app.callback())
+      .get('/')
+      .set('Origin', 'http://eggjs-white.org')
+      .expect('Access-Control-Allow-Origin', app.config.cors.origin)
+      .expect('access-control-allow-credentials', 'true')
+      .expect(200);
+  });
+});
---
+++ test/fixtures/apps/cors.origin/config/config.default.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.keys = 'foo';
+
+exports.cors = {
+  origin: 'eggjs.org',
+  credentials: true,
+};
+
+exports.security = {
+  domainWhiteList: [
+    'eggjs-white.org',
+  ],
+};
