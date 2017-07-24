424c56472d5982c02e78bf48b8b74025b2524437
TZ | 天猪 <atian25@qq.com>
2017-07-24 11:28:41 +0800

    deps: update deps (#8)

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ module.exports = app => {
   // put before other core middlewares
   app.config.coreMiddlewares.unshift('cors');
 
-  // if security plugin enabled, and no origin config provided, only allow safe domains support CORS.
+  // if security plugin enabled, and origin config is not provided, will only allow safe domains support CORS.
   app.config.cors.origin = app.config.cors.origin || function corsOrigin(ctx) {
-    const origin = ctx.get('Origin');
+    const origin = ctx.get('origin');
     if (!ctx.isSafeDomain || ctx.isSafeDomain(origin)) {
       return origin;
     }
