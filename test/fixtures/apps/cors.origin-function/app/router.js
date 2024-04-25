e4eec61f1e33eb02cb9358b60d4bd206d1a49b8d
fengmk2 <suqian.yf@antgroup.com>
2024-04-25 23:22:07 +0800

    fix: let framework known app has custom origin handler or not
    auto set config.cors.hasCustomOriginHandler

--- test/fixtures/apps/cors.origin-function/app/router.js
+++ test/fixtures/apps/cors.origin-function/app/router.js
@@ -7,6 +7,12 @@ module.exports = app => {
     };
   });
 
+  app.get('/config', async ctx => {
+    ctx.body = {
+      hasCustomOriginHandler: ctx.app.config.cors.hasCustomOriginHandler,
+    };
+  });
+
   app.post('/', async ctx => {
     ctx.body = {
       foo: 'bar',
